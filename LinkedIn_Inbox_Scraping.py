0373b3e4d5f3bada5f6bd26c1543977ad81192db
mehdimujtaba <48270298+mehdimujtaba@users.noreply.github.com>
2019-08-20 22:30:05 -0700

    Update LinkedIn_Inbox_Scraping.py

--- LinkedIn_Inbox_Scraping.py
+++ LinkedIn_Inbox_Scraping.py
@@ -1,47 +1,57 @@
-import pandas as pd
-import pyzmail
-import imapclient
+import time
+import re
+from selenium import webdriver
 
-###Setting up connection to gmail and reading all emails
-imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
-imapObj.login('mehdi.******@gmail.com', 'password')
-imapObj.select_folder('INBOX', readonly=True)
+# Browser
+driver = webdriver.Chrome() # Optional argument, if not specified will search path.
+driver.get(‘https://www.linkedin.com/login')
 
-###Searching for mail by subject keywords. Each mail is then given an ID 
-UIDs = imapObj.search(['OR','OR','OR','OR','OR','OR','OR','OR','OR','SUBJECT','data','SUBJECT','science','SUBJECT','analyst','SUBJECT','sql','SUBJECT','recruiting','SUBJECT','career','SUBJECT','job','SUBJECT','phone','SUBJECT','analytics','SUBJECT','opening'])  
+nameidElem = driver.find_element_by_id(‘username’)
+nameidElem.send_keys(‘*****@gmail.com’)
+
+pwdidElem = driver.find_element_by_id(‘password’)
+pwdidElem.send_keys(‘*******’)
+continueElem = driver.find_element_by_class_name(“btn__primary — large”) 
+result=continueElem.submit()
+
+time.sleep(10)
+
+messagesElem = driver.find_element_by_id(“messaging-tab-icon”) 
+messagesElem.click()
+
 sender_email_list=[]
-sender_name_list=[]
-
-###Looping through relevant mails and extracting email address and first name
-for i in UIDs:
-  rawMessages = imapObj.fetch([i], ['BODY[]', 'FLAGS'])
-  message = pyzmail.PyzMessage.factory(rawMessages[i][b'BODY[]'])
-  sender_details=message.get_addresses('from')
-  #Extracting email
-  sender_email=sender_details[0][1]
-  #Extracting first name
-  sender_name =sender_details[0][0].split()[0] 
-  #Adding to list
-  sender_email_list=sender_email_list+[sender_email]
-  sender_name_list =sender_name_list +[sender_name]
-
-###Joining email and name columns and exporting as csv for editing
-list_subject=pd.DataFrame(list(zip(sender_email_list,sender_name_list)))
-list_subject.to_csv('email_name_list.csv',index=False)
-
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.connect('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-server.ehlo()
-server.login('mehdi.******@gmail.com', 'password')
-
-###Reading edited list of emails and first names
-list_subject=pd.read_csv('list_subject.csv')
-
-###Looping through list and sending email
-for i in range(0,len(list_subject)):
-  message = 'Subject: {}\n\n{}'.format('Reconnecting..', 'Hi %s,\n\nWe were in touch regarding some openings a while back. Just wanted to let you know I am available currently and came across some great roles which I would be interested in discussing.\n\nRegards,\nMehdi Mujtaba' %list_subject.iloc[i][1])
-  server.sendmail('mehdi.******@gmail.com', '%s' %list_subject.iloc[i][0] ,message)
-
+current_edit=[]
+current_edit_cum=[]
+for i in range(0,10):
+  print(i)  
+  current=driver.find_elements_by_class_name("msg-conversation-listitem__participant-names")
+  current_edit_cum=current_edit_cum+current_edit
+  current_edit=list(set(current) - set(current_edit_cum))
+  for person in current_edit:
+      person.click()
+      print(person.text)
+      time.sleep(5)
+      for text in driver.find_elements_by_class_name("msg-s-event-listitem__body"):
+        emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text.text)
+        if emails!=[]:
+            sender_email_list=sender_email_list+[emails]
+            time.sleep(10)
+  person.click()
+  scroll=driver.find_element_by_tag_name('html')
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  scroll.send_keys(Keys.DOWN)
+  
+import numpy as np
+sender_email_list=np.unique(sender_email_list)
+list_subject=pd.DataFrame(list(sender_email_list))
+list_subject.to_csv('C:\\Users\\User\\Downloads\\linkedin_list.csv',index=False)
